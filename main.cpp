06119f0f6b114b2bcac6db85dbab021a46dca48d
ACking-you <2394626220@qq.com>
2021-08-19 21:35:00 +0800

    修改了接口中的参数规范，方便操作套接字

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
+#include <fstream>
 #include "TCP_SOCKET_CLIENT.h"
 
-using namespace std;
 
 int main() {
     TCP_SOCKET_CLIENT a;
-    const char *URL = "github.com";
-    a.Gethostbyname(URL);
-
+    a.Gethostbyname("github.com");
 }
