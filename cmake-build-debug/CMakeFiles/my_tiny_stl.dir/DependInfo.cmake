8bc62d946e7e2f32cd9b879b35076b4e2d1e452f
ACking-you <2394626220@qq.com>
2022-02-25 15:55:16 +0800

    update

--- cmake-build-debug/CMakeFiles/my_tiny_stl.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/my_tiny_stl.dir/DependInfo.cmake
@@ -1,15 +1,14 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
   )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "D:/CLion Project/my_tiny_stl/main.cpp" "D:/CLion Project/my_tiny_stl/cmake-build-debug/CMakeFiles/my_tiny_stl.dir/main.cpp.obj"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
 
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/CLion Project/my_tiny_stl/main.cpp" "CMakeFiles/my_tiny_stl.dir/main.cpp.obj" "gcc" "CMakeFiles/my_tiny_stl.dir/main.cpp.obj.d"
   )
 
 # Targets to which this target links.
--- cmake-build-debug/CMakeFiles/my_tiny_stl.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/my_tiny_stl.dir/cmake_clean.cmake
@@ -1,5 +1,6 @@
 file(REMOVE_RECURSE
   "CMakeFiles/my_tiny_stl.dir/main.cpp.obj"
+  "CMakeFiles/my_tiny_stl.dir/main.cpp.obj.d"
   "libmy_tiny_stl.dll.a"
   "my_tiny_stl.exe"
   "my_tiny_stl.exe.manifest"
